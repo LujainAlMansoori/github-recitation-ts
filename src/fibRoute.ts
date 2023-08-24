576f0d7619ceef68b8f8b0a71bef10d2a9a50d7d
Mohammad <mannan@andrew.cmu.edu>
2023-08-24 14:37:05 +0300

    Fix so that no further lint errors with file src/fibRoute.ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -3,9 +3,10 @@ import { Request} from 'express';
 import fibonacci  from './fib';
 
 export default (req: Request, res) => {
+  console.log(res)
   const { num }  = req.params;
-
-  const fibN = fibonacci(parseInt(num)) as number;
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+  const fibN: number = fibonacci(parseInt(num));
   let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
